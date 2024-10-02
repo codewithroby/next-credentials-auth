02d77d738a959ab0c3144e3cfba5c98d752eb24a
Roby Codes <robert.cfx@proton.me>
2024-10-03 01:26:38 +0200

    refactor(schema): Changed schema name into camelCase.

--- src/schemas/register-schema.ts
+++ src/schemas/register-schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const RegisterSchema = z.object({
+export const registerSchema = z.object({
   name: z.string({
     required_error: "Name is required.",
     invalid_type_error: "Name must be a string.",
