35abecb00c21c763535ee0e91badb2dc5fcbc0a6
Roby Codes <robert.cfx@proton.me>
2024-10-03 01:59:01 +0200

    feat(config): Added config file.

---
+++ src/lib/config.ts
@@ -0,0 +1,8 @@
+const appDomain = "http://localhost:3000";
+
+export const appConfig = {
+  name: "Next 14 Credentials Auth",
+  description:
+    "Next 14 Auth is a Next.js starter template with credentials authentication built using NextAuth v5, Drizzle ORM, PostgreSQL, TypeScript, Zod and Resend.",
+  metaImage: `${appDomain}/api/og`,
+} as const;
