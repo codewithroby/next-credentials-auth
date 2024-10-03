40ec47bec43f924f46d7ffb76246400619e7a5f6
Roby Codes <robert.cfx@proton.me>
2024-10-03 02:17:32 +0200

    feat(config): Imported appData from config for SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import { type Metadata } from "next";
 import { Inter } from "next/font/google";
+import { appConfig } from "@/lib/config";
 import "./globals.css";
 
 const AppFont = Inter({
   subsets: ["latin"],
 });
 
+const { name, description, metaImage } = appConfig;
+
 export const metadata: Metadata = {
-  title: "Next 14 Credentials Auth",
-  description:
-    "Next 14 Auth is a Next.js starter template with credentials authentication built using NextAuth v5, Drizzle ORM, PostgreSQL, TypeScript, Zod and Resend.",
+  title: name,
+  description: description,
   openGraph: {
-    images: "/api/og",
+    images: metaImage,
   },
 };
 
