24ab5d135f26374dc9938966f860e549061aaef0
Roby Codes <robert.cfx@proton.me>
2024-10-03 02:14:58 +0200

    feat(t3-env): Added environment variables validation library.

--- src/env.ts
+++ src/env.ts
@@ -5,13 +5,13 @@ export const env = createEnv({
   server: {
     DOMAIN: z.string().url(),
     AUTH_SECRET: z.string(),
-    AUTH_TRUST_HOST: z.string().url(), // This is needed to make next-auth work in applications behind a proxy
+    AUTH_TRUST_HOST: z.string().url().optional(), // This is needed to make next-auth work in applications behind a proxy
   },
 
   runtimeEnv: {
     DOMAIN: process.env.DOMAIN,
     AUTH_SECRET: process.env.AUTH_SECRET,
-    AUTH_TRUST_HOST: process.env.DOMAIN,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
   },
 
   emptyStringAsUndefined: true,
