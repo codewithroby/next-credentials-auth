05b09499f6e321c404d13f2252e44f31e4859a03
Roby Codes <robert.cfx@proton.me>
2024-10-03 02:12:13 +0200

    feat(env): Added domain name from env variable.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,9 @@
-const appDomain = "http://localhost:3000";
+import { env } from "@/env";
 
 export const appConfig = {
+  domain: env.DOMAIN,
   name: "Next 14 Credentials Auth",
   description:
     "Next 14 Auth is a Next.js starter template with credentials authentication built using NextAuth v5, Drizzle ORM, PostgreSQL, TypeScript, Zod and Resend.",
-  metaImage: `${appDomain}/api/og`,
+  metaImage: `${env.DOMAIN}/api/og`,
 } as const;
