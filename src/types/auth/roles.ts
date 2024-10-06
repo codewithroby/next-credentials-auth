e1c3ec4451d8d5c71520237e84a616a0356a61a4
Roby Codes <robert.cfx@proton.me>
2024-10-06 17:36:59 +0200

    fix(roles type): corrected roles type.

--- src/types/auth/roles.ts
+++ src/types/auth/roles.ts
@@ -1,3 +1,6 @@
 import { USER_ROLES } from "@/constants/roles";
 
-export type UserRoleType = typeof USER_ROLES;
+export type UserRoleType =
+  | typeof USER_ROLES.GUEST
+  | typeof USER_ROLES.ADMIN
+  | typeof USER_ROLES.SUPER_ADMIN;
