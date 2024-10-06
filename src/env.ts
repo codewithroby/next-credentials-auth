7a77966d24450727ace7330e4aff341330a0bc5c
Roby Codes <robert.cfx@proton.me>
2024-10-06 17:26:35 +0200

    env(server): added DATABASE_URL variable.

--- src/env.ts
+++ src/env.ts
@@ -3,15 +3,17 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DOMAIN: z.string().url(),
-    AUTH_SECRET: z.string(),
-    AUTH_TRUST_HOST: z.string().url().optional(), // This is needed to make next-auth work in applications running from behind a proxy
+    DOMAIN: z.string().min(1).url(),
+    AUTH_SECRET: z.string().min(1),
+    AUTH_TRUST_HOST: z.string().min(1).url().optional(), // This is needed to make next-auth work in applications running from behind a proxy
+    DATABASE_URL: z.string().min(1),
   },
 
   runtimeEnv: {
     DOMAIN: process.env.DOMAIN,
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 
   emptyStringAsUndefined: true,
