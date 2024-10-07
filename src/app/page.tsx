63c88b8e40b4bcedbc579af3f9a12b0e3bfaaea9
Roby Codes <robert.cfx@proton.me>
2024-10-08 00:04:05 +0200

    feat(auth): added auth dialog button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-const HomePage = async () => <div>maniaz</div>;
+import { AuthDialogButton } from "@/components/auth-dialog-btn";
+
+const HomePage = async () => (
+  <main>
+    <section className="min-h-screen flex justify-center items-center">
+      <AuthDialogButton />
+    </section>
+  </main>
+);
 
 export default HomePage;
