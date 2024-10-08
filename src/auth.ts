e91ed9672b4bdda9e82fa49ce3dfa9cf2efb6e2e
Roby Codes <robert.cfx@proton.me>
2024-10-08 02:29:48 +0200

    refactor(auth): merged auth config files.

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,25 @@
-import NextAuth, { type NextAuthConfig } from "next-auth";
-import { AuthConfig } from "@/auth/auth.config";
+import NextAuth, { type User, NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { Provider } from "next-auth/providers";
+
+const Providers: Provider[] = [
+  Credentials({
+    authorize: async () => {
+      const user: User = {
+        name: "Roby",
+      };
+
+      return user;
+    },
+  }),
+];
+
+const AuthConfig = {
+  providers: Providers,
+  session: {
+    strategy: "jwt",
+  },
+} satisfies NextAuthConfig;
 
 export const {
   handlers: { GET, POST },
--- src/auth/auth.config.ts
+++
@@ -1,9 +0,0 @@
-import { type NextAuthConfig } from "next-auth";
-import { Providers } from "@/auth/auth.providers";
-
-export const AuthConfig = {
-  providers: Providers,
-  session: {
-    strategy: "jwt",
-  },
-} satisfies NextAuthConfig;
