bff47baaf430f4873415e6974a0b93e12b08229a
Roby Codes <robert.cfx@proton.me>
2024-10-08 02:24:43 +0200

    refactor(roles): optimized UserRoleType value.

--- src/types/auth/roles.ts
+++ src/types/auth/roles.ts
@@ -1,6 +1,3 @@
 import { USER_ROLES } from "@/constants/roles";
 
-export type UserRoleType =
-  | typeof USER_ROLES.GUEST
-  | typeof USER_ROLES.ADMIN
-  | typeof USER_ROLES.SUPER_ADMIN;
+export type UserRoleType = (typeof USER_ROLES)[keyof typeof USER_ROLES];
