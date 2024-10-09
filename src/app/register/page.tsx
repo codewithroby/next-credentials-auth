82d4a6f0d8aaf035fcb623b9d2e420dbadcc47d1
Roby Codes <robert.cfx@proton.me>
2024-10-09 21:29:51 +0200

    feat(page): created registration page.

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components/auth";
+
+const RegisterPage = () => (
+  <main>
+    <section className="min-h-screen flex justify-center items-center">
+      <RegisterForm />
+    </section>
+  </main>
+);
+
+export default RegisterPage;
