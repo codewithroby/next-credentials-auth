2d7d4e108a9158aa8f4d81d158c2c78d6fcd1388
Roby Codes <robert.cfx@proton.me>
2024-10-09 18:20:07 +0200

    fix(jwt): fixed error unused var on jwt.

--- src/types/auth/next-auth.d.ts
+++ src/types/auth/next-auth.d.ts
@@ -1,20 +1,18 @@
-import { type DefaultSession } from "next-auth";
+import { JWT as DefaultJWT } from "next-auth/jwt";
 import { accounts } from "@/database/schemas/account";
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      role: typeof accounts.$inferSelect.role;
-    } & DefaultSession["user"];
-  }
-
   interface User {
     role: typeof accounts.$inferSelect.role;
+  }
+
+  interface Session {
+    user: User;
   }
 }
 
 declare module "next-auth/jwt" {
-  interface JWT {
+  interface JWT extends DefaultJWT {
     role: typeof accounts.$inferSelect.role;
   }
 }
