da9968c08f14e036c9b265f8a13dad95fbc6a9cc
Roby Codes <robert.cfx@proton.me>
2024-10-09 21:44:55 +0200

    feat(page): created registration page.

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,7 @@ const RegisterPage = () => (
   <main>
     <section className="min-h-screen flex justify-center items-center">
       <div className="container sm:max-w-[400px] px-6 sm:px-0">
-        <div className="flex flex-col space-y-4 bg-white px-6 py-8 rounded-md shadow-sm">
+        <div className="flex flex-col space-y-4 bg-white p-6 rounded-md shadow-sm">
           <RegisterForm />
         </div>
       </div>
