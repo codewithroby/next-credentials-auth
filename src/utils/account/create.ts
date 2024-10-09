b31590a16d00abf361a617f7eb8e123143ce0a0f
Roby Codes <robert.cfx@proton.me>
2024-10-09 18:02:58 +0200

    feat(utils): completed account creation utility function.

--- src/utils/account/create.ts
+++ src/utils/account/create.ts
@@ -1,9 +1,10 @@
+import bcrypt from "bcryptjs";
 import { type RegisterSchemaType } from "@/types/auth/register";
 import { findAccountByEmail } from "@/utils/account/find-by-email";
 import { validateNewAccount } from "@/utils/validators/validate-new-account";
 import { db } from "@/database/client";
-import bcrypt from "bcryptjs";
 import { accounts } from "@/database/schemas/account";
+import { login } from "@/app/actions/auth";
 
 export const createAccount = async (values: RegisterSchemaType) => {
   const { name, email, password } = validateNewAccount(values);
@@ -13,11 +14,18 @@ export const createAccount = async (values: RegisterSchemaType) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const account = await db.insert(accounts).values({
-    name,
-    email,
-    password: hashedPassword,
-  });
+  const account = await db
+    .insert(accounts)
+    .values({
+      name,
+      email,
+      password: hashedPassword,
+    })
+    .returning();
 
-  console.log(account);
+  if (account[0])
+    login({
+      email: account[0].email ?? "",
+      password: account[0].password ?? "",
+    });
 };
