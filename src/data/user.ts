ab074f78349b66308c64e8fea838430e9dc038b5
Roby Codes <robert.cfx@proton.me>
2024-10-12 00:22:35 +0200

    data(user): created getUserById helper.

---
+++ src/data/user.ts
@@ -0,0 +1,16 @@
+import { eq } from "drizzle-orm";
+import { db } from "@/database/client";
+import { accounts } from "@/database/schemas/account";
+
+export const getUserById = async (id: string) => {
+  const account = await db
+    .select({
+      role: accounts.role,
+    })
+    .from(accounts)
+    .where(eq(accounts.id, id))
+    .limit(1)
+    .execute();
+
+  return account[0];
+};
