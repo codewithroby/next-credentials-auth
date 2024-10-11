b2789fa41a5ac9eee1a023ac3b2f40d24cbcbf56
Roby Codes <robert.cfx@proton.me>
2024-10-12 00:21:56 +0200

    config(database): set all account fields as not null.

--- src/database/schemas/account.ts
+++ src/database/schemas/account.ts
@@ -3,9 +3,12 @@ import { UserRoleType } from "@/types/auth/roles";
 import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }),
-  password: varchar("password", { length: 255 }),
-  role: varchar("role").$type<UserRoleType>().default(USER_ROLES.USER),
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull(),
+  password: varchar("password", { length: 255 }).notNull(),
+  role: varchar("role")
+    .$type<UserRoleType>()
+    .default(USER_ROLES.USER)
+    .notNull(),
 });
