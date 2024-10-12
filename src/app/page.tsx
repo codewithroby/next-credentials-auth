05b5f41a26a2fbcff39d0a9b678b2d345d10fb94
Roby Codes <robert.cfx@proton.me>
2024-10-12 03:36:01 +0200

    style(page): added dashboard-card component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,12 @@
-import { logout } from "@/app/actions/auth";
-import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { DashboardCard } from "@/components/dashboard-card";
 
-const LogOutForm = () => (
-  <form action={logout}>
-    <Button className="uppercase font-medium" type="submit">
-      Log Out
-    </Button>
-  </form>
-);
-
-const HomePage = async () => {
-  const session = await auth();
-
+const HomePage = () => {
   return (
     <main>
-      <section className="min-h-screen flex justify-center items-center flex-col space-y-4">
-        {session?.user.id ? (
-          <>
-            <span className="uppercase font-semibold">User ID</span>
-            <span className="text-sm">{session.user.id}</span>
-            <LogOutForm />
-          </>
-        ) : (
-          <Link href="/login">
-            <Button className="uppercase font-medium">Login</Button>
-          </Link>
-        )}
+      <section className="min-h-screen flex justify-center items-center">
+        <DashboardCard title="Homepage">
+          <span>hey</span>
+        </DashboardCard>
       </section>
     </main>
   );
