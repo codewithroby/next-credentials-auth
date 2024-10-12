ec24b3df736c2f846a866989efb210c6beaf354b
Roby Codes <robert.cfx@proton.me>
2024-10-12 13:51:04 +0200

    style(role): added role based badge color.

--- src/components/profile-info-pill.tsx
+++ src/components/profile-info-pill.tsx
@@ -1,4 +1,8 @@
-const ProfileInfoPill = ({
+import { USER_ROLES } from "@/constants/roles";
+import { UserRoleType } from "@/types/auth/roles";
+import { cn } from "@/utils/class-name";
+
+export const ProfileInfoPill = ({
   title,
   content,
 }: {
@@ -7,10 +11,32 @@ const ProfileInfoPill = ({
 }) => (
   <div className="flex rounded-md shadow-sm border border-gray-100 bg-white py-4 px-3 space-x-3 items-center">
     <span className="flex-1 uppercase font-semibold text-sm">{title}</span>
-    <span className="uppercase text-xs text-nowrap overflow-hidden text-ellipsis max-w-[300px] text-right bg-primary text-white rounded-sm py-1 px-2 font-medium">
+    <span className="uppercase text-xs text-nowrap overflow-hidden text-ellipsis max-w-[200px] text-right bg-primary text-white rounded-sm py-1 px-2 font-medium">
       {content}
     </span>
   </div>
 );
 
-export { ProfileInfoPill };
+export const RoleInfoPill = ({
+  title,
+  content,
+}: {
+  title: string;
+  content: UserRoleType;
+}) => (
+  <div className="flex rounded-md shadow-sm border border-gray-100 bg-white py-4 px-3 space-x-3 items-center">
+    <span className="flex-1 uppercase font-semibold text-sm">{title}</span>
+    <span
+      className={cn(
+        "uppercase text-xs text-nowrap overflow-hidden text-ellipsis max-w-[200px] text-right text-white rounded-sm py-1 px-2 font-medium",
+        {
+          "bg-green-600": (content = USER_ROLES.USER),
+          "bg-sky-600": (content = USER_ROLES.MODERATOR),
+          "bg-indigo-600": (content = USER_ROLES.ADMIN),
+        }
+      )}
+    >
+      {content}
+    </span>
+  </div>
+);
