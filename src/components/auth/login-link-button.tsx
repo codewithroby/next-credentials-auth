1f580d9c4c910b01cc657ac05d9333efd651c861
Roby Codes <robert.cfx@proton.me>
2024-10-12 03:38:56 +0200

    feat(component): created login link button component.

--- src/components/auth/login-link-button.tsx
+++ src/components/auth/login-link-button.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 const LoginLinkButton = () => (
-  <Link href="/login" aria-label="login button">
-    <Button>Login</Button>
+  <Link href="/login" aria-label="login button" className="flex justify-center">
+    <Button className="uppercase w-[150px]">Login</Button>
   </Link>
 );
 
