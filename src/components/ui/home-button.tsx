8f777ee30c4bd220f9b8b14e96465b5a83f51d9a
Roby Codes <robert.cfx@proton.me>
2024-10-12 04:19:57 +0200

    feat(component): created home button component.

---
+++ src/components/ui/home-button.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export const HomeButton = () => (
+  <Link href="/" aria-label="homepage" className="fixed bottom-10 right-10">
+    <Button className="rounded-full size-14 p-3.5">
+      <svg
+        className="size-full"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="28"
+        height="28"
+        color="#ffffff"
+        fill="none"
+      >
+        <path
+          d="M3.16405 11.3497L4 11.5587L4.45686 16.1005C4.715 18.6668 4.84407 19.9499 5.701 20.7249C6.55793 21.5 7.84753 21.5 10.4267 21.5H13.5733C16.1525 21.5 17.4421 21.5 18.299 20.7249C19.1559 19.9499 19.285 18.6668 19.5431 16.1005L20 11.5587L20.836 11.3497C21.5201 11.1787 22 10.564 22 9.85882C22 9.35735 21.7553 8.88742 21.3445 8.59985L13.1469 2.86154C12.4583 2.37949 11.5417 2.37949 10.8531 2.86154L2.65549 8.59985C2.24467 8.88742 2 9.35735 2 9.85882C2 10.564 2.47993 11.1787 3.16405 11.3497Z"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M15 16C14.2005 16.6224 13.1502 17 12 17C10.8498 17 9.79952 16.6224 9 16"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+        />
+      </svg>
+    </Button>
+  </Link>
+);
