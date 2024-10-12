f24e819b867bdfd75e1f8412874d0c7baedb5ed0
Roby Codes <robert.cfx@proton.me>
2024-10-12 13:32:45 +0200

    feat(user): added setUserRoleById utility function.

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,16 +1,32 @@
 import { eq } from "drizzle-orm";
 import { db } from "@/database/client";
 import { accounts } from "@/database/schemas/account";
+import { UserRoleType } from "@/types/auth/roles";
 
 export const getUserById = async (id: string) => {
-  const account = await db
+  const user = await db
     .select({
       role: accounts.role,
     })
     .from(accounts)
     .where(eq(accounts.id, id))
-    .limit(1)
-    .execute();
+    .limit(1);
 
-  return account[0];
+  return user[0];
+};
+
+export const setUserRoleById = async (id: string, role: UserRoleType) => {
+  const user = await db
+    .update(accounts)
+    .set({
+      role: role,
+    })
+    .where(eq(accounts.id, id))
+    .returning({
+      id: accounts.id,
+    });
+
+  if (user[0].id) return true;
+
+  return false;
 };
