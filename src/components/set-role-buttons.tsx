3471e252a6d2d9d718739380836d09d47b9af20c
Roby Codes <robert.cfx@proton.me>
2024-10-12 14:45:17 +0200

    feat(component): created set-role-form component.

--- src/components/set-role-buttons.tsx
+++ src/components/set-role-buttons.tsx
@@ -1,53 +1,16 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
 import {
   setAdminRole,
   setModeratorRole,
   setRegularRole,
 } from "@/app/actions/user";
-import { useRouter } from "next/navigation";
+import { SetRoleForm } from "./set-role-form";
 
 export const SetRoleButtons = () => {
-  const router = useRouter();
-
   return (
     <div className="flex space-x-2">
-      <form
-        action={async () => {
-          await setRegularRole().then(() => {
-            router.refresh();
-          });
-        }}
-        className="flex-1"
-      >
-        <Button
-          type="submit"
-          className="w-full uppercase bg-green-600 hover:bg-green-600/90"
-        >
-          Regular User
-        </Button>
-      </form>
-      <Button
-        className="flex-1 uppercase bg-sky-600 hover:bg-sky-600/90"
-        onClick={async () => {
-          await setModeratorRole().then(() => {
-            router.refresh();
-          });
-        }}
-      >
-        Moderator
-      </Button>
-      <Button
-        className="flex-1 uppercase bg-indigo-600 hover:bg-indigo-600/90"
-        onClick={async () => {
-          await setAdminRole().then(() => {
-            router.refresh();
-          });
-        }}
-      >
-        Admin
-      </Button>
+      <SetRoleForm setRole={setRegularRole} role="REGULAR USER" />
+      <SetRoleForm setRole={setModeratorRole} role="MODERATOR" />
+      <SetRoleForm setRole={setAdminRole} role="ADMIN" />
     </div>
   );
 };
--- src/components/set-role-form.tsx
+++ src/components/set-role-form.tsx
@@ -6,6 +6,8 @@ import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { ImSpinner9 } from "react-icons/im";
 import { UserRoleType } from "@/types/auth/roles";
+import { cn } from "@/utils/class-name";
+import { USER_ROLES } from "@/constants/roles";
 
 export const SetRoleForm = ({
   setRole,
@@ -29,7 +31,11 @@ export const SetRoleForm = ({
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1">
           <Button
             type="submit"
-            className="w-full uppercase relative"
+            className={cn("w-full uppercase relative", {
+              "bg-green-600 hover:bg-green-600/90": role === USER_ROLES.USER,
+              "bg-sky-600 hover:bg-sky-600/90": role === USER_ROLES.MODERATOR,
+              "bg-indigo-600 hover:bg-indigo-600/90": role === USER_ROLES.ADMIN,
+            })}
             disabled={form.formState.isSubmitting}
           >
             {form.formState.isSubmitting ? (
