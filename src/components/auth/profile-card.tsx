158d870f1dd77da2c12d9a84421d9a77cd1b3faa
Roby Codes <robert.cfx@proton.me>
2024-10-12 04:00:30 +0200

    feat(component): created profile card component.

---
+++ src/components/auth/profile-card.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { ProfileData } from "./profile-data";
+
+const ProfileCard = async () => {
+  const user = await useCurrentUser();
+
+  return (
+    <div className="container sm:max-w-lg px-6 sm:px-0">
+      <div className="flex flex-col space-y-4 bg-white p-6 rounded-md shadow-sm">
+        {user?.id ? (
+          <div className="flex flex-col space-y-4">
+            <span className="font-bold uppercase text-xl text-center">
+              Profile
+            </span>
+            <ProfileData user={user} />
+          </div>
+        ) : (
+          <div className="flex flex-col space-y-4 items-center">
+            <span className="font-semibold uppercase">
+              You Are Not Logged In
+            </span>
+            <Link href="/login">
+              <Button className="uppercase font-medium w-[120px]">Login</Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export { ProfileCard };
