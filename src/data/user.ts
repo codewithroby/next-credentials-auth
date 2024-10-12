800b73123d44f9f27b53fc352d90610e7cb184d1
Roby Codes <robert.cfx@proton.me>
2024-10-12 14:30:18 +0200

    remove(file): removed set role file.

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,7 +1,6 @@
 import { eq } from "drizzle-orm";
 import { db } from "@/database/client";
 import { accounts } from "@/database/schemas/account";
-import { UserRoleType } from "@/types/auth/roles";
 
 export const getUserById = async (id: string) => {
   const user = await db
@@ -13,20 +12,4 @@ export const getUserById = async (id: string) => {
     .limit(1);
 
   return user[0];
-};
-
-export const setUserRoleById = async (id: string, role: UserRoleType) => {
-  const user = await db
-    .update(accounts)
-    .set({
-      role: role,
-    })
-    .where(eq(accounts.id, id))
-    .returning({
-      id: accounts.id,
-    });
-
-  if (user[0].id) return true;
-
-  return false;
 };
