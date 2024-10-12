13331760243ae3074b15bbfe2f455411c081ff9e
Roby Codes <robert.cfx@proton.me>
2024-10-12 03:45:59 +0200

    config(database): updated database schema.

--- src/database/schemas/migrations/0000_petite_iron_lad.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "accounts" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar(255),
-	"email" varchar(255),
-	"password" varchar(255),
-	"role" varchar DEFAULT 'USER'
-);
